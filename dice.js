f6978129dc15898be67947d2f0453aed5479d6c3
Vaibhav <vvaibhav@apple.com>
2020-09-18 21:15:54 -0700

    added pgn and fen

--- dice.js
+++ dice.js
@@ -1,48 +1,37 @@
+var board = null
+var game = new Chess()
+var dicesToMove = []
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
 
-// Initialization
-var board = null;
-var game = new Chess ();
-var inProgress = true;
-var dicesToMove = [];
 
-// Functions
-function bookKeeping() {
-  if (game.in_draw() || game.in_stalemate() || game.in_threefold_repetition()) {
-    document.getElementById('turn').innerHTML = "Nobody wins! It's a draw";
-    inProgress = false;
-    return;
+function resetBoard () {
+  var config = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd
   }
-  dicesToMove = [];
-  if (game.turn() === 'b') {
-    if (game.in_checkmate()) {
-      inProgress = false;
-      document.getElementById('turn').innerHTML = "White wins! Black is checkmated";
-    } else {
-      dicesToMove = rollDices();
-      document.getElementById('turn').innerHTML = "Black to play";
-      document.getElementById('dice').innerHTML = "";
-      dicesToMove.forEach(piece => document.getElementById('dice').innerHTML += "<img src=img/chesspieces/wikipedia/b" + piece + ".png>");
-    }
-  } else {
-    if (game.in_checkmate()) {
-      inProgress = false;
-      document.getElementById('turn').innerHTML = "Black wins! White is checkmated";
-    } else {
-      dicesToMove = rollDices();
-      document.getElementById('turn').innerHTML = "White to play";
-      document.getElementById('dice').innerHTML = "";
-      dicesToMove.forEach(piece => document.getElementById('dice').innerHTML += "<img src=img/chesspieces/wikipedia/w" + piece + ".png>");
-    }
-  }
+  board = Chessboard('myBoard', config)
+  game = new Chess()
+  dicesToMove = []
+  var currPiece = null
+  updateStatus()
+}
+
+function flip () {
+  board.flip()
 }
 
 function isValidRoll(piece) {
   var moves = game.moves();
   for (var i=0; i<moves.length; i++) {
-    if (moves[i].length == 2 && piece === 'P') {
+    if (moves[i].charAt(0).toLowerCase() === moves[i].charAt(0) && piece === 'P') {
       return true;
     }
-    if (moves[i].length == 2 && piece === moves[i].charAt(0)) {
+    if (moves[i].charAt(0).toUpperCase() === moves[i].charAt(0) && piece === moves[i].charAt(0)) {
       return true;
     }
   }
@@ -58,45 +47,86 @@ function rollDices() {
   return [map[throws[0]], map[throws[1]], map[throws[2]]];
 }
 
-function resetBoard() {
-  board = Chessboard('board', config);
-  game = new Chess ();
-  inProgress = true;
-  bookKeeping();
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // only pick up pieces for the side to move
+  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+  }
+  currPiece = piece;
 }
 
-function onDrop (source, target, piece, newPos, oldPos, orientation) {
-  // console.log('Source: ' + source)
-  // console.log('Target: ' + target)
-  // console.log('Piece: ' + piece)
-  // console.log('New position: ' + Chessboard.objToFen(newPos))
-  // console.log('Old position: ' + Chessboard.objToFen(oldPos))
-  // console.log('Orientation: ' + orientation)
-  // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
+function onDrop (source, target) {
   var currTurn = game.turn();
-  var isValidMove = game.move({ from: source, to: target, promotion: 'q'});
-  if (isValidMove === null || !inProgress) {
-    // don't move
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+  if (!(currTurn + dicesToMove[0] === currPiece || currTurn + dicesToMove[1] === currPiece || currTurn + dicesToMove[2] === currPiece)) {
+    game.undo();
     return 'snapback';
   }
-  // if (!(currTurn + dicesToMove[0] === piece || currTurn + dicesToMove[1] === piece || currTurn + dicesToMove[2] === piece)) {
-  //   game.undo();
-  //   return 'snapback';
-  // }
-
-  bookKeeping();
+  updateStatus()
 }
 
-
-// Game
-var config = {
-  draggable: true,
-  position: 'start',
-  onDrop: onDrop
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
 }
 
-board = Chessboard('board', config);
-bookKeeping();
+function updateStatus () {
+  var status = ''
+
+  var moveColor = 'White'
+  if (game.turn() === 'b') {
+    moveColor = 'Black'
+  }
+
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.'
+  }
+
+  // draw?
+  else if (game.in_draw() || game.in_stalemate() || game.in_threefold_repetition()) {
+    status = 'Game over, drawn position'
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move'
+
+    // check?
+    if (game.in_check()) {
+      status += ', ' + moveColor + ' is in check'
+    }
+
+    // Roll dices
+    dicesToMove = rollDices();
+    document.getElementById('dice').innerHTML = "";
+
+    if (game.turn() === 'b') {
+      dicesToMove.forEach(piece => document.getElementById('dice').innerHTML += "<img src=img/chesspieces/wikipedia/b" + piece + ".png>")
+    } else {
+      dicesToMove.forEach(piece => document.getElementById('dice').innerHTML += "<img src=img/chesspieces/wikipedia/w" + piece + ".png>")
+    }
+  }
+
+  $status.html(status)
+  $fen.html(game.fen())
+  $pgn.html(game.pgn())
+}
+
+resetBoard()
+
 $('#startPositionBtn').on('click', resetBoard)
-$('#flipOrientationBtn').on('click', board.flip)
+$('#flipOrientationBtn').on('click', flip)
